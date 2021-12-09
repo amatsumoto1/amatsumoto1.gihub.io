9c363e5200d06f3c26fc09a54d7278ab4ef5bc34
amatsumoto1 <amatsumoto95@gmail.com>
2021-12-09 08:37:45 -0700

    Added canvas hook

---
+++ src/hooks/useCanvas.ts
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from 'react';
+
+const useCanvas = (draw: (context: CanvasRenderingContext2D, frameCount: number) => void) => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            canvasCtxRef.current = canvasRef.current.getContext('2d');
+            let ctx = canvasCtxRef.current;
+            let frameCount = 0;
+            let animationFrameId: number;
+
+            const render = () => {
+                frameCount++;
+                draw(ctx, frameCount);
+                animationFrameId = window.requestAnimationFrame(render);
+            };
+
+            render();
+
+            return () => {
+                window.cancelAnimationFrame(animationFrameId);
+            };
+        }
+    }, [draw]);
+
+    return canvasRef;
+}
+
+export default useCanvas;
